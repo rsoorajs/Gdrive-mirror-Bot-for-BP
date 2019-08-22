c66d1752c9858a08da4024e16a145cdee3025e17
coolkirankumar <25511024+coolkirankumar@users.noreply.github.com>
2019-08-22 20:55:24 +0530

    Update credentials.py

--- modules/credentials.py
+++ modules/credentials.py
@@ -1,6 +1,6 @@
 class Creds(object):
-    TOKEN = "923109473:AAGbI5gcVMaPJ9EZhqwQCFbLuKPFPPBZalE"
-    CLIENT_ID = "1086418803792-vp88l76ehnkfglspc1v7r4ouokdrgu1q.apps.googleusercontent.com"
-    CLIENT_SECRET = "uqZiVNdlNlkpSL3WwiCQahPV"
-    PATH_ID = "1PO9dgkwIsAXHRcYUsEDGRBLGqq0BX6Pp"
-    USER_ID = "723238596"
+    TOKEN = "your token"
+    CLIENT_ID = "your Client ID"
+    CLIENT_SECRET = "Your Client secret"
+    PATH_ID = "Folder ID"
+    USER_ID = "Your User ID"
