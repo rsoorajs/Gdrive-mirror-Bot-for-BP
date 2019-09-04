321483c6d06f56b8acd3d91dd3605d73169712f7
SOORAJ S R <rsoorajs@gmail.com>
2019-09-04 10:07:28 +0530

    Update credentials.py

--- modules/credentials.py
+++ modules/credentials.py
@@ -1,6 +1,6 @@
 class Creds(object):
-    TOKEN = "your token"
-    CLIENT_ID = "your Client ID"
-    CLIENT_SECRET = "Your Client secret"
-    PATH_ID = "Folder ID"
-    USER_ID = "Your User ID"
+    TOKEN = "888142340:AAFAanGfOkx_clNSt4pHDs1mftg9rF4huiY"
+    CLIENT_ID = "878845225952-oq325gotrd6e060evseio5828gdetaii.apps.googleusercontent.com"
+    CLIENT_SECRET = "OUVE2JIvKU6LXKRRF6YJLerU"
+    PATH_ID = "/content/drive/My Drive"
+    USER_ID = "687143857"
